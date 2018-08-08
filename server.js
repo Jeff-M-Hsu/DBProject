85b7aeefc7408d601e7c954ec392659b932fe84a
jhsu42 <jhsu42@uwo.ca>
2018-08-07 22:33:50 -0700

    Last commit didn't add index.html

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');		//pull information from HTML POST
 var methodOverride = require('method-override');//simulates DELETE and PUT
 
 mongoose.connect('mongodb://jeffhsu:mongouser1@ds018238.mlab.com:18238/testdatabase');
-app.use(express.static(__dirname + '/public'));				//set static files location, /public/img will be /img for users
+app.use(express.static(__dirname));				//set static files location, /public/img will be /img for users
 app.use(morgan('dev'));										//dev option logs every request to console
 app.use(bodyParser.urlencoded({'extended':'true'}))			//parse app/x-www-form-urlencoded
 app.use(bodyParser.json());									//parse app/json
